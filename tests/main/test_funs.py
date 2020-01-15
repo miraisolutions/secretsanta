9ee23f76aafbb34a847059a0f443c3efcc25e50a
spoltier <simon.poltier@mirai-solutions.com>
2020-01-15 15:27:14 +0100

    #10 add allowed char classes in given, format

--- tests/main/test_funs.py
+++ tests/main/test_funs.py
@@ -5,10 +5,11 @@ from hypothesis.strategies import text, sets, integers, characters
 
 
 class MakeSantaDict(unittest.TestCase):
-    @given(sets(text(alphabet=characters(whitelist_categories=["Lu", "Ll"]), min_size=3, max_size=20), 2, 10),
+    @given(sets(text(alphabet=characters(whitelist_categories=["Lu", "Ll", "Nd", "Pc", "Pd"],
+                                         whitelist_characters=["@"]), min_size=3, max_size=20), 2, 10),
            integers(min_value=0, max_value=2**32 - 1))
     def test_no_self_assign(self, test_set, seed):
-        test_list=list(test_set)
+        test_list = list(test_set)
         test_dict = dict(zip(test_list, test_list))
         assignment = funs.make_santa_dict(test_dict, seed)
         for left, right in assignment.items():
