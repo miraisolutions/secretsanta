6fd17280ebd19ab1fb6b3bcee683cbd08a29b284
spoltier <simon.poltier@mirai-solutions.com>
2021-04-08 12:20:37 +0200

    Proposal for simpler shuffle algo

--- secretsanta/main/funs.py
+++ secretsanta/main/funs.py
@@ -1,10 +1,9 @@
 import datetime
 import logging
 import smtplib
-from contextlib import suppress
 from secretsanta.main.core import SecretSanta
 from secretsanta.main.utils import setup_logging
-from typing import Optional, Dict, Tuple, List, Union, Any
+from typing import Optional, Dict, Tuple, List, Union
 
 import numpy as np
 
@@ -13,8 +12,12 @@ import numpy as np
 
 # mypy is missing a library stub file for module 'numpy' and will complain about it
 # workaround: append --ignore-missing-imports to the mypy call (see https://github.com/python/mypy/issues/3905)
-def make_santa_dict(dictionary: Dict[str, str], seed: Optional[int] = None,
-                    verbose: bool = False, level: str = "ERROR") -> Dict[str, str]:
+def make_santa_dict(
+    dictionary: Dict[str, str],
+    seed: Optional[int] = None,
+    verbose: bool = False,
+    level: str = "ERROR",
+) -> Dict[str, str]:
     # type triple-quotes and press enter to generate empty docstring stub
     """Creates a random secret santa assignment as a dictionary from an initial dictionary of the participants' names
     and associated email addresses.
@@ -30,14 +33,7 @@ def make_santa_dict(dictionary: Dict[str, str], seed: Optional[int] = None,
     # Implementation note: the main challenge we have beyond simply shuffling a list is to ensure no one gets assigned
     # as their own secret santa. This means we need to exclude the participant from the secret santa choices as we go
     # through the list of participants.
-    # However the last e-mail left to be assigned may be precisely the last participant's.
-    # To see this, consider:
-    # 1. we start with participants [a,b,c] and their e-mails [a@acme.com, b@acme.com, c@acme.com]
-    # 2. random assignment of a to b@acme.com; a@acme.com and c@acme.com are left
-    # 3. random assignment of b to a@acme.com; only c@acme.com is left
-    # 4. only c is left, and only c's e-mail is left! We can only assign c to him/herself
-    # If this situation occurs, we simply swap the last two entries, so e.g. c's assignment would be changed to
-    # a@acme.com and b's to c@acme.com.
+    # in order to obtain this property, we rotate sub-sections of the list of participants, instead of shuffling it.
     ######
 
     setup_logging(level)
@@ -47,14 +43,9 @@ def make_santa_dict(dictionary: Dict[str, str], seed: Optional[int] = None,
     # We need to map each person to some e-mail address, so we start by getting a list of all names
     # unpack dict_keys object into list literal (no control over order!)
     # https://stackoverflow.com/questions/16819222/how-to-return-dictionary-keys-as-a-list-in-python
-    names = [*dictionary.keys()]
+    names: List[str] = [*dictionary.keys()]
 
     # To store the shuffled dictionary, we initialize a new variable
-    senddict: Dict[Union[str, Any], Union[str, Any]] = {}
-    # To avoid the last assignee be one's own secret santa, we may need to swap the two last entries; therefore we need
-    # to keep track of the second to last one. This variable must be defined here to avoid warnings about undefined
-    # names.
-    swapname1 = ''
 
     np.random.seed(seed)
 
@@ -65,45 +56,60 @@ def make_santa_dict(dictionary: Dict[str, str], seed: Optional[int] = None,
     if len(dictionary) <= 3:
         logger.warning("Too few people, assignment will be deterministic")
 
-    # "dict"s are always unordered, therefore iterating through them has unpredictable order
-    for name in dictionary:
-        # print(dictionary.get(name))
-        if verbose:
-            logger.info(str(name))
-        pick = names.copy()
-        if len(pick) == 1:
-            # if this is the last person in the list, we only have one choice left
-            picked = pick[0]
-            # we check if the last person was the last available choice and if so, swap the last 2 entries
-            if picked == name:
-                swapname2 = picked
-                tmp = senddict[swapname1]
-                senddict[swapname1] = dictionary[picked]
-                senddict[swapname2] = tmp
-            else:
-                senddict[name] = dictionary[picked]
-        else:
-            # if only 2 choices are left we keep track of the name, in case the last e-mail left is the last
-            # participant's
-            if len(pick) == 2:
-                swapname1 = name
-            # try:
-            #     pick.remove(name)
-            # except ValueError:
-            #     pass
-            # the above try-except is equivalent to:
-            with suppress(ValueError):
-                # A person shouldn't be their own secret santa, so we remove them as a possible pick
-                pick.remove(name)
-            # randomly pick a participant
-            picked = np.random.choice(pick, 1)[0]
-            if verbose:
-                logger.info(str(picked))
-            # set `name`'s value in the result to the picked participant's e-mail.
-            senddict[name] = dictionary[picked]
-            names.remove(picked)
+    shuffled_names = shuffle_move_all(names)
 
-    return senddict
+    return dict(zip(shuffled_names, dictionary.values()))
+
+
+def pivots(lst):
+    """
+    Randomly generate an ordered list of unique valid indices, \
+    defining segments of length two or greater over the input list.
+    :param lst: the list to split
+    :return: random, ordered indices of the list, including 0 and the length.
+    """
+    pivot = 0
+    end = len(lst)
+    result = []
+    # the last pivot must allow for a segment of length >=2 up to the end
+    while pivot <= end - 2:
+        result = result + [pivot]
+        # computing the next pivot fails if the pivot is = end-2, as the min and max args of randint are the same;
+        # so we check and stop if the next pivot would be thrown away by the next iteration (pivot + 2 > end-2)
+        if pivot <= end - 4:
+            pivot = np.random.randint(pivot + 2, end)
+        else:
+            break
+    return result + [end]
+
+
+def rotate(n, lst):
+    """
+    rotate the list by a given number of steps.
+    :param n: steps to rotate the list by
+    :param lst: list to rotate
+    :return: the rotated list
+    """
+    return lst[n:] + lst[:n]
+
+
+def shuffle_move_all(lst):
+    """
+    randomly shuffle a list such that none of the elements remains in its previous index.
+    :param lst: the list to shuffle
+    :return: the shuffled list
+    """
+    # We start by creating pivots delimiting segments of length at least 2
+    pvts = pivots(lst)
+    for i in range(0, len(pvts) - 1):
+        start = pvts[i]
+        stop = pvts[i + 1]
+        sublen = stop - start - 1
+        # each segment of the list gets rotated by at least one, up to (not including) its length; this guarantees that
+        # no element will remain in its original position.
+        rotate_steps = 1 if sublen <= 1 else np.random.randint(1, sublen)
+        lst[start:stop] = rotate(rotate_steps, lst[start:stop])
+    return lst
 
 
 def send_santa_dict(smtpserverwithport: str, sender: str, pwd: str,
