6d9b0e17f2bf390d0cf87520afdcd87559a3c35e
farani <nicoletta.farabullini@mirai-solutions.com>
2021-06-08 10:00:08 +0200

    Fix for test_info

--- tests/main/test_logging.py
+++ tests/main/test_logging.py
@@ -2,11 +2,11 @@ import unittest
 import os
 from hypothesis import given
 from secretsanta.main.funs import make_santa_dict
-from hypothesis.strategies import lists, integers, sampled_from
+from hypothesis.strategies import text, lists, integers, characters, sampled_from
 
 class TestLogging(unittest.TestCase):
     # test error
-    def test_error(self):
+    def test_error_single_participant(self):
         participants_dict = {'Stephanie': 'Stephanie'}
         os.environ["level"] = "ERROR"
         log_level = os.environ.get('log_level', os.getenv("level")).upper()
@@ -15,9 +15,9 @@ class TestLogging(unittest.TestCase):
 
     # test warning
     ls = ['Stephanie', 'Simon', 'Gustavo']
-    @given(lists(sampled_from(ls), min_size=2, max_size=2, unique=True),
-           integers(min_value=1, max_value=1))
-    def test_warning(self, participants, seed):
+    @given(lists(sampled_from(ls), min_size=2, max_size=2, unique=True))
+
+    def test_warning(self, participants):
         # convert list to dict
         participants_dict = {i: i for i in participants}
         # check if directory exists, if not then create it
@@ -29,17 +29,23 @@ class TestLogging(unittest.TestCase):
         os.environ["level"] = "DEBUG"
         log_level = os.environ.get('log_level', os.getenv("level")).upper()
         with self.assertLogs('secretsanta.main.funs', level='WARNING') as cm:
-            make_santa_dict(participants_dict, seed=seed, verbose=True, level=log_level)
-        self.assertEqual(cm.output[0], 'WARNING:secretsanta.main.funs:WARNING: Too few people, assignment will be deterministic')
+            make_santa_dict(participants_dict, seed=1, verbose=True, level=log_level)
+        self.assertEqual(cm.output[0], 'WARNING:secretsanta.main.funs:Too few people, assignment will be deterministic')
 
     # test info
-    ls = ['Stephanie', 'Simon', 'Gustavo', 'Nicola', 'Guido', 'Francesca',
-          'Gabriel', 'Roland', 'Nikki', 'Peter', 'Martin', 'Riccardo']
-    @given(lists(sampled_from(ls), min_size=4, max_size=4, unique=True),
-           integers(min_value=1, max_value=1))
-    def test_info(self, participants, seed):
+    #ls = ['Stephanie', 'Simon', 'Gustavo', 'Nicola', 'Guido', 'Francesca',
+    #      'Gabriel', 'Roland', 'Nikki', 'Peter', 'Martin', 'Riccardo']
+
+    @given(lists(text(alphabet=characters(whitelist_categories=["Lu", "Ll", "Nd", "Pc", "Pd"],
+                                              whitelist_characters=["@"]), min_size=2, max_size=20),
+                     min_size=2, max_size=10, unique=True),
+        integers(min_value=1, max_value=1))
+    #@given(lists(sampled_from(ls), min_size=4, max_size=4, unique=True),
+    #       integers(min_value=1, max_value=1))
+
+    def test_info(self, test_list, seed):
         #convert list to dict
-        participants_dict = {i: i for i in participants}
+        participants_dict = {i: i for i in test_list}
         # check if directory exists, if not then create it
         check_dir = os.path.exists('log_files')
         if not check_dir:
@@ -48,6 +54,7 @@ class TestLogging(unittest.TestCase):
             os.makedirs(path)
         os.environ["level"] = "DEBUG"
         log_level = os.environ.get('log_level', os.getenv("level")).upper()
-        with self.assertLogs('secretsanta.main.funs', level='INFO') as cm:
-            make_santa_dict(participants_dict, seed = seed, verbose = True, level = log_level)
-        self.assertEqual(cm.output[0], 'INFO:secretsanta.main.funs:INFO:' + participants[0])
+        if log_level == "INFO":
+            with self.assertLogs('secretsanta.main.funs', level='INFO') as cm:
+                make_santa_dict(participants_dict, seed=seed, verbose=True, level=log_level)
+            self.assertEqual(cm.output[0], log_level + ':secretsanta.main.funs:' + test_list[0])
