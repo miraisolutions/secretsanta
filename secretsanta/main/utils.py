4ae8e7376bb81ae66508099e6f92b0d98cf94db9
farani <nicoletta.farabullini@mirai-solutions.com>
2021-06-15 12:57:34 +0200

    Remove return statement

--- secretsanta/main/utils.py
+++ secretsanta/main/utils.py
@@ -4,5 +4,5 @@ import os
 def setup_logging(level: str = "ERROR"):
     name_file = 'secretsanta_' + time.strftime("%Y%m%d-%H%M%S") +'.log'
     path_file = os.path.join('./log_files/', name_file)
-    return logging.basicConfig(filename =path_file, level = level, format = '%(asctime)s %(levelname)s %(message)s',
+    logging.basicConfig(filename =path_file, level = level, format = '%(asctime)s %(levelname)s %(message)s',
                                datefmt = '%Y/%m/%d %I:%M:%S %p')
