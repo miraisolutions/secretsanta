3b9eefe1f762c8ed8966f19535ac1a8fedb48541
farani <nicoletta.farabullini@mirai-solutions.com>
2021-06-18 08:27:55 +0200

    added docstring to utils file

--- secretsanta/main/utils.py
+++ secretsanta/main/utils.py
@@ -2,6 +2,12 @@ import logging
 import time
 import os
 def setup_logging(level: str = "ERROR"):
+    """
+        sets up logging configuration.
+
+        :param level: logging level
+    """
+
     name_file = 'secretsanta_' + time.strftime("%Y%m%d-%H%M%S") +'.log'
     path_file = os.path.join('./log_files/', name_file)
     logging.basicConfig(filename =path_file, level = level, format = '%(asctime)s %(levelname)s %(message)s',
