975a7b8aaa19775b38f19529822f08794b3ea823
Simon Poltier <simon.poltier@mirai-solutions.com>
2025-04-22 17:46:26 +0200

    CI

--- noxfile.py
+++ noxfile.py
@@ -21,6 +21,29 @@ def lint(sess):
 
 
 @session(venv_backend="uv")
+def lint_critical(sess):
+    sess.install(".", "flake8")
+    # stop the build if there are Python syntax errors or undefined names
+    sess.run(
+        "flake8",
+        ".",
+        "--count",
+        "--select=E9,F63,F7,F82",
+        "--show-source",
+        "--statistics",
+    )
+
+
+@session(venv_backend="uv")
+def lint_style(sess):
+    sess.install(".", "flake8")
+    # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
+    sess.run(
+        "flake8", ".", "--count", "--exit-zero", "--max-complexity=10", "--statistics"
+    )  # max-line-length is now in pyproject.toml
+
+
+@session(venv_backend="uv")
 def docs(sess):
     sess.install(".", *proj["project"]["optional-dependencies"]["dev"])
     sess.run("sphinx-build", "docs/source", "docs/build/html")
