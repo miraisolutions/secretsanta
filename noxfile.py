c8ea6bcb7ae3762e99eeeeef42ac54d714d0963c
Simon Poltier <simon.poltier@mirai-solutions.com>
2025-04-22 17:22:41 +0200

    nox: use uv backend

--- noxfile.py
+++ noxfile.py
@@ -3,20 +3,24 @@ from nox import session
 
 # load project deps from pyproject.toml
 import nox.project
+
 proj = nox.project.load_toml("pyproject.toml")
 
-@session(python=["3.8","3.9","3.10","3.11","3.12"])
+
+@session(python=["3.8", "3.9", "3.10", "3.11", "3.12"], venv_backend="uv")
 def tests(sess):
     # install runtime + dev deps
     sess.install(".", *proj["project"]["optional-dependencies"]["dev"])
     sess.run("pytest", "--cov=secretsanta")
 
-@session
+
+@session(venv_backend="uv")
 def lint(sess):
     sess.install(".", "flake8")
     sess.run("flake8", "secretsanta", "tests")
 
-@session
+
+@session(venv_backend="uv")
 def docs(sess):
     sess.install(".", *proj["project"]["optional-dependencies"]["dev"])
     sess.run("sphinx-build", "docs/source", "docs/build/html")
