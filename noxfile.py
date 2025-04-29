0b3fc11e1638c88d627a77f7d35a82d054120d57
Simon Poltier <simon.poltier@mirai-solutions.com>
2025-04-29 18:26:56 +0200

    use ruff instead of flake8 + dev dependencies

--- noxfile.py
+++ noxfile.py
@@ -10,46 +10,43 @@ proj = nox.project.load_toml("pyproject.toml")
 @session(python=["3.8", "3.9", "3.10", "3.11", "3.12"], venv_backend="uv")
 def tests(sess):
     # install runtime + dev deps
-    sess.install(".", *proj["project"]["optional-dependencies"]["dev"])
+    sess.install(".", *proj["dependency-groups"]["dev"])
     sess.run("pytest", "--cov=secretsanta")
 
 
 @session(venv_backend="uv")
 def lint(sess):
-    sess.install(".", "flake8")
-    sess.run("flake8", "secretsanta", "tests")
+    sess.install(".", "ruff")
+    sess.run("ruff", "check")
+    sess.run("ruff", "format", "--check")
 
 
-@session(venv_backend="uv")
+@session(venv_backend="uv", default=False)
 def lint_critical(sess):
-    sess.install(".", "flake8")
+    sess.install(".", "ruff")
     # stop the build if there are Python syntax errors or undefined names
     sess.run(
-        "flake8",
+        "ruff",
+        "check",
         ".",
-        "--count",
         "--select=E9,F63,F7,F82",
-        "--show-source",
         "--statistics",
     )
 
 
-@session(venv_backend="uv")
+@session(venv_backend="uv", default=False)
 def lint_style(sess):
-    sess.install(".", "flake8")
-    # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
-    sess.run(
-        "flake8", ".", "--count", "--exit-zero", "--max-complexity=10", "--statistics"
-    )  # max-line-length is now in pyproject.toml
+    sess.install(".", "ruff")
+    sess.run("ruff", "check", "--exit-zero", "--statistics")
 
 
 @session(venv_backend="uv")
 def docs(sess):
-    sess.install(".", *proj["project"]["optional-dependencies"]["dev"])
+    sess.install(".", *proj["dependency-groups"]["dev"])
     sess.run("sphinx-build", "docs/source", "docs/build/html")
 
 
 @session(venv_backend="uv")
 def doctest(sess):
-    sess.install(".", *proj["project"]["optional-dependencies"]["dev"])
+    sess.install(".", *proj["dependency-groups"]["dev"])
     sess.run("sphinx-build", "-b", "doctest", "docs/source", "docs/build/doctest")
